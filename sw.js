d996ab97a8a78f5d917ba99058c58c513aeca73b
bgpwmhkbw5-blip <bgpwmhkbw5@privaterelay.appleid.com>
2026-06-13 10:46:34 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "knowledge-discord-v5";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./style.css?v=5",
+    "./db.js?v=5",
+    "./app.js?v=5",
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(ASSETS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys()
+            .then((keys) => Promise.all(
+                keys
+                    .filter((key) => key !== CACHE_NAME)
+                    .map((key) => caches.delete(key))
+            ))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request)
+            .then((cached) => cached || fetch(event.request))
+    );
+});
